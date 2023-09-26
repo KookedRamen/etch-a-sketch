79e3ef7c9f82aa46fc18902bdf8bef23dd213ad8
Travis <travis.thundershield@gmail.com>
2023-09-26 11:39:32 -0400

    added javascript functionality for drawing

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+for (let i = 0; i < 256; i++){
+    const div = document.createElement('div')
+    const container = document.querySelector('.grid')
+    div.classList.add('box')
+    // div.textContent = 'Test'
+    
+    
+
+    container.appendChild(div)
+}
+function addClass(e){
+    
+}
+const divs = document.querySelectorAll('.box')
+divs.forEach((item) =>{
+    item.addEventListener('mouseover click', (e) => {
+        e.target.style.backgroundColor = 'black'
+    })
+})
