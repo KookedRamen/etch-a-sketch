a3afea84fd75bc8f22d6dfd583b7feab3d70eb95
Travis <travis.thundershield@gmail.com>
2023-09-27 08:07:18 -0400

    started loop for user specified grid

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ for (let i = 0; i < 256; i++){
 
     container.appendChild(div)
 }
-function addClass(e){
-    
-}
+
 const divs = document.querySelectorAll('.box')
 divs.forEach((item) =>{
     item.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'black'
     })
+})
+
+const btn = document.querySelector('.gridSize')
+btn.addEventListener('click', function(){
+    let gridSide = prompt('What size grid would you like?')
+    for (let i = 0; i < gridSide; i++){
+        const div = document.createElement('div')
+        const container = document.querySelector('.grid')
+        div.classList.add('box')
+        // div.textContent = 'Test'
+        
+        
+    
+        container.appendChild(div)
+        divs.forEach((item) =>{
+            item.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'black'
+            })
+        })
+        
+    }
 })
